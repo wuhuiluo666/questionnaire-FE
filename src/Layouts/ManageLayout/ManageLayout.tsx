c44c9b09cb431e1bc6715c6fac53ed97b68d626e
wuhuiluo666 <510426343@qq.com>
2023-07-20 09:27:02 +0800

    Add Create newQuestion Api

--- src/Layouts/ManageLayout/ManageLayout.tsx
+++ src/Layouts/ManageLayout/ManageLayout.tsx
@@ -4,15 +4,19 @@ import { Outlet } from 'react-router-dom'
 import { Button, Divider, Space } from 'antd'
 import { PlusOutlined, BarsOutlined, StarOutlined, DeleteOutlined } from '@ant-design/icons'
 import { useLocation, useNavigate } from 'react-router-dom'
-
+import { createNewQuestion } from '../../services/question'
 
 const ManageLayout = () => {
     const { pathname } = useLocation()
     const nav = useNavigate()
+    const createQuestion = async () => {
+        const data = await createNewQuestion()
+        nav(`/question/edit/${data.id}`)
+    }
     return <div className={styles.container}>
         <div className={styles.left}>
             <Space direction={'vertical'}>
-                <Button icon={<PlusOutlined />} size={'large'} type={'default'}>创建问卷</Button>
+                <Button onClick={createQuestion} icon={<PlusOutlined />} size={'large'} type={'default'}>创建问卷</Button>
                 <Divider />
                 <Button icon={<BarsOutlined />} onClick={() => nav('/manage/list')} size={'large'} type={pathname.startsWith('/manage/list') ? 'primary' : 'default'}>我的问卷</Button>
                 <Button icon={<StarOutlined />} onClick={() => nav('/manage/star')} size={'large'} type={pathname.startsWith('/manage/star') ? 'primary' : 'default'}>星标问卷</Button>
--- src/services/question.ts
+++ src/services/question.ts
@@ -1,7 +1,15 @@
 import instance, { ResDataType } from './ajax'
 
+// 获取问卷详情
 export const getQuestionDetail = async (id: string) => {
     const url = `/api/question/${id}`
     const data = (await instance.get(url)) as ResDataType
+    return data
+}
+
+// 创建问卷
+export const createNewQuestion = async () => {
+    const url = '/api/question'
+    const data = (await instance.post(url)) as ResDataType
     return data
 }
