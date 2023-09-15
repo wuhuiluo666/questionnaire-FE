b397006e9731082a2d9c7fa163610e9452705e1a
wuhuiluo666 <510426343@qq.com>
2023-09-15 17:20:45 +0800

    Complete Static Page Skelon

--- src/pages/question/static/Index.tsx
+++ src/pages/question/static/Index.tsx
@@ -4,26 +4,38 @@ import { useGetPageInfo } from '../../../hooks/useGetPageInfo'
 import { useGetQuestionDetail } from '../../../hooks/useLoadQuestionData'
 import { useNavigate } from 'react-router-dom'
 import { StaticHeader } from './header'
+import styles from './index.module.scss'
 
 
 const Static = () => {
     const { loading } = useGetQuestionDetail()
     const { title, isPublished } = useGetPageInfo()
     const nav = useNavigate()
-    if (loading) {
-        return (
-            <div style={{ textAlign: 'center', marginTop: '100px' }}>
-                <Spin />
-            </div>
-        )
+    const LoadingElement = (
+        <div style={{ textAlign: 'center', marginTop: '100px' }}>
+            <Spin />
+        </div>
+    )
+    const genContentElement = () => {
+        // 未发布
+        // if (typeof isPublished === 'boolean' && !isPublished) {
+        //     return <Result style={{ margin: '0 auto' }} status={'warning'} title={'问卷暂未发布'} extra={<Button type={'primary'} onClick={() => nav(-1)}>返回上一页</Button>}>
+        //     </Result>
+        // }
+        return <>
+            <div className={styles.left}>Left</div>
+            <div className={styles.main}>Main</div>
+            <div className={styles.right}>Right</div>
+        </>
     }
-    // 未发布
-    // if (!isPublished) {
-    //     return <Result status={'warning'} title={'问卷暂未发布'} extra={<Button type={'primary'} onClick={() => nav(-1)}>返回上一页</Button>}>
-    //     </Result>
-    // }
-    return <div>
+    return <div className={styles.container}>
         <StaticHeader />
+        <div className={styles['content-wrapper']}>
+            {loading && LoadingElement}
+            {!loading && <div className={styles.content}>
+                {genContentElement()}
+            </div>}
+        </div>
     </div>
 }
 
--- src/pages/question/static/header.tsx
+++ src/pages/question/static/header.tsx
@@ -1,15 +1,41 @@
-import React from 'react'
-import { EditOutlined, LeftOutlined } from '@ant-design/icons'
-import { Button, Space, Typography } from 'antd'
+import React, { useRef } from 'react'
+import { CopyOutlined, EditOutlined, LeftOutlined, QrcodeOutlined } from '@ant-design/icons'
+import { Button, Input, Popover, Space, Tooltip, Typography, InputRef, message } from 'antd'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useGetPageInfo } from '../../../hooks/useGetPageInfo'
+import QRCode from 'qrcode.react'
 import styles from './header.module.scss'
 
 
 export const StaticHeader = () => {
     const { id } = useParams()
-    const { title } = useGetPageInfo()
+    const { title, isPublished } = useGetPageInfo()
     const nav = useNavigate()
+    const linkInputRef = useRef<InputRef>(null)
+    const copyLink = () => {
+        const ele = linkInputRef.current
+        if (ele === null) return
+        // 选中input内容
+        ele.select()
+        // 执行富文本编辑器
+        document.execCommand('copy')
+        message.success('链接已复制道剪贴板')
+    }
+    const genLinkAndQrCodeEle = () => {
+        // 如果未发布
+        // if (!isPublished) return null // 暂时注释掉
+        // 拼接url
+        const url = `http://localhost:3000/question/${id}`
+        return <Space>
+            <Input ref={linkInputRef} value={url} style={{ width: '300px' }} />
+            <Tooltip title={'拷贝链接'}>
+                <Button onClick={copyLink} icon={<CopyOutlined />}></Button>
+            </Tooltip>
+            <Popover content={<QRCode value={url} size={150}></QRCode>}>
+                <Button icon={<QrcodeOutlined />}></Button>
+            </Popover>
+        </Space>
+    }
     return <div className={styles['header-wrapper']}>
         <div className={styles.header}>
             <div className={styles.left}>
@@ -20,7 +46,7 @@ export const StaticHeader = () => {
                     <Typography.Title style={{ marginTop: 0 }} level={5}>{title}</Typography.Title>
                 </Space>
             </div>
-            <div className={styles.main}>123</div>
+            <div className={styles.main}>{genLinkAndQrCodeEle()}</div>
             <div className={styles.right}>
                 <Button onClick={() => nav(`/question/edit/${id}`)} type={'primary'} icon={<EditOutlined />}>
                     编辑问卷
