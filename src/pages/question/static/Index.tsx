1461f0ec833d5232c53ad93ad0c10c687ac699eb
wuhuiluo666 <510426343@qq.com>
2023-09-18 17:27:35 +0800

    完成饼图和柱状图

--- src/pages/question/static/Index.tsx
+++ src/pages/question/static/Index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
+import React, { useMemo, useState } from 'react'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend, PieChart, Pie, Cell } from 'recharts'
 import { Result, Spin, Button, Table, Typography } from 'antd'
 import { useGetPageInfo } from '../../../hooks/useGetPageInfo'
 import { useGetQuestionDetail } from '../../../hooks/useLoadQuestionData'
@@ -11,8 +11,47 @@ import { useRequest } from 'ahooks'
 import { getStaticListService } from '../../../services/static'
 import { useGetComponentsList } from '../../../hooks/useGetComponentsList'
 
+const BarList: { name: string, count: number }[] = [
+    {
+        name: '11',
+        count: 222
+    },
+    {
+        name: '12',
+        count: 242
+    },
+    {
+        name: '13',
+        count: 232
+    },
+    {
+        name: '14',
+        count: 122
+    },
+]
+
+export const STAT_COLORS = [
+    '#2468F2',
+    '#A5E693',
+    '#FAD000',
+    '#F33E3E',
+    '#A985FF',
+    '#005C99',
+    '#87D26D',
+    '#FF8E52',
+    '#E62E6B',
+    '#98B8FF',
+]
 
 const Static = () => {
+    const pieList = [
+        { name: '饼图1', count: 1 },
+        { name: '饼图2', count: 2 },
+        { name: "饼图3", count: 3 }
+    ]
+    const sum = useMemo(() => {
+        return pieList.reduce((prev, cur) => prev + cur.count, 0)
+    }, [pieList])
     const { id = '' } = useParams()
     const [list, setList] = useState([])
     const [total, setTotal] = useState(0)
@@ -49,8 +88,7 @@ const Static = () => {
             )
         }
     })
-    console.log('columns', columns)
-    console.log('list', list)
+
     const genContentElement = () => {
         // 未发布
         // if (typeof isPublished === 'boolean' && !isPublished) {
@@ -66,9 +104,9 @@ const Static = () => {
                 <Table rowKey={(c: any) => c._id} dataSource={list} columns={columns} loading={loadingTb} pagination={false} />
             </div>
             <div className={styles.right}>
-                <div style={{ width: 400, height: 300 }}>
+                {/* <div style={{ width: 400, height: 300 }}>
                     <ResponsiveContainer height={'100%'} width={'100%'}>
-                        <BarChart width={400} height={300} margin={{
+                        <BarChart data={BarList} width={400} height={300} margin={{
                             top: 5,
                             right: 30,
                             left: 0,
@@ -78,8 +116,22 @@ const Static = () => {
                             <XAxis dataKey={'name'} />
                             <YAxis />
                             <Tooltip />
+                            <Legend />
                             <Bar dataKey={'count'} fill="#82ca9d" />
                         </BarChart>
+                    </ResponsiveContainer>
+                </div> */}
+                <div style={{ width: 400, height: 400 }}>
+                    <ResponsiveContainer width={'100%'} height={'100%'}>
+                        <PieChart width={400} height={400}>
+                            <Pie label={i => `${i.name} - ${Number((i.count / sum).toFixed(2)) * 100}%`} outerRadius={50} cx={'50%'} cy={'50%'} data={pieList} dataKey={'count'}>
+                                {
+                                    pieList.map((p, index) => {
+                                        return <Cell key={index} fill={STAT_COLORS[index]}></Cell>
+                                    })
+                                }
+                            </Pie>
+                        </PieChart>
                     </ResponsiveContainer>
                 </div>
             </div>
