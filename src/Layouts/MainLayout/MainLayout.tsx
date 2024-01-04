7154d21229c09eecd0c6238415ee00e6ffeffd29
A reacter <136437794+wuhuiluo666@users.noreply.github.com>
2024-01-05 00:12:18 +0800

    Update MainLayout.tsx

--- src/Layouts/MainLayout/MainLayout.tsx
+++ src/Layouts/MainLayout/MainLayout.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './MainLayout.module.scss'
 import { Outlet } from 'react-router-dom'
 import { Layout, Spin } from 'antd'
 import Logo from '../../components/Logo/Logo'
 import UserInfo from '../../components/UserInfo/UserInfo'
-import { useGetUserInfo } from '../../hooks/useGetUserInfo'
-import { useNav } from '../../hooks/useNav'
+// import { useGetUserInfo } from '../../hooks/useGetUserInfo'
+// import { useNav } from '../../hooks/useNav'
 const { Header, Content, Footer } = Layout
 
 const MainLayout = () => {
-    const { loading } = useGetUserInfo()
-    useNav(loading)
+    const [loading,setLoading] = useState(false)
+    // const { loading } = useGetUserInfo()
+    // useNav(loading)
     return (
         <Layout>
             <Header className={styles.header}>
