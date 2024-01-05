af2896571bf18db34994a3c44c2e9b374934d04e
A reacter <136437794+wuhuiluo666@users.noreply.github.com>
2024-01-05 15:02:24 +0800

    Update question.ts

--- src/services/question.ts
+++ src/services/question.ts
@@ -10,14 +10,14 @@ interface SearchType {
 
 // 获取问卷详情
 export const getQuestionDetail = async (id: string): Promise<ResDataType> => {
-  const url = `https://question-server.onrender.com/api/question/${id}`
+  const url = `https://huy.wiki:3002/api/question/${id}`
   const data = (await instance.get(url)) as ResDataType
   return data
 }
 
 // 创建问卷
 export const createNewQuestion = async (): Promise<ResDataType> => {
-  const url = 'https://question-server.onrender.com/api/question'
+  const url = 'https://huy.wiki:3002/api/question'
   const data = (await instance.post(url)) as ResDataType
   return data
 }
@@ -26,7 +26,7 @@ export const createNewQuestion = async (): Promise<ResDataType> => {
 export const getQuestionList = async (
   searchParams: Partial<SearchType> = {}
 ): Promise<ResDataType> => {
-  const url = 'https://question-server.onrender.com/api/question'
+  const url = 'https://huy.wiki:3002/api/question'
   const data = (await instance.get(url, {
     params: searchParams
   })) as ResDataType
@@ -38,21 +38,21 @@ export const changeQuestionStar = async (
   id: string,
   option: { [key: string]: any }
 ): Promise<ResDataType> => {
-  const url = `https://question-server.onrender.com/api/question/${id}`
+  const url = `https://huy.wiki:3002/api/question/${id}`
   const data = (await instance.patch(url, option)) as ResDataType
   return data
 }
 
 // 复制问卷
 export const copyQuestion = async (id: string): Promise<ResDataType> => {
-  const url = `https://question-server.onrender.com/api/question/copy/${id}`
+  const url = `https://huy.wiki:3002/api/question/copy/${id}`
   const data = (await instance.post(url)) as ResDataType
   return data
 }
 
 // 删除问卷
 export const deleteQuestion = async (ids: string[]) => {
-  const url = 'https://question-server.onrender.com/api/question'
+  const url = 'https://huy.wiki:3002/api/question'
   const data = (await instance.delete(url, {
     data: ids
   })) as ResDataType
