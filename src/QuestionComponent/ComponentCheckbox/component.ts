d0b712a6baa9ef64bec09289052f82a7b6e949dc
A reacter <136437794+wuhuiluo666@users.noreply.github.com>
2024-01-05 16:44:05 +0800

    Update component.ts

--- src/QuestionComponent/ComponentCheckbox/component.ts
+++ src/QuestionComponent/ComponentCheckbox/component.ts
@@ -1,10 +1,12 @@
 import { ComponentCheckBox, defaultProps } from ".";
 import { CheckBoxProps } from "./CheckProps";
+import StatComponent from "./CheckboxStatic"
 
 export const ComponentCheckBoxConfig = {
     title: '复选框',
     type: 'QuestionCheckbox',
     Component: ComponentCheckBox,
+    ChartComp: StatComponent,
     ComponentProps: CheckBoxProps,
     defaultProps: defaultProps 
 }
