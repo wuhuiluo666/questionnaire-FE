8a6ea93819f95a388e30f5a1dc7bd39b4c6ce7e1
A reacter <136437794+wuhuiluo666@users.noreply.github.com>
2024-01-05 15:03:24 +0800

    Update user.ts

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,7 +1,7 @@
 import instance, { ResDataType } from './ajax'
 
 export const getUserInfo = async () : Promise<ResDataType> => {
-  const url = 'https://question-server.onrender.com/api/user/info'
+  const url = 'https://huy.wiki:3002/api/user/info'
   const data = (await instance.get(url)) as ResDataType
   return data
 }
@@ -10,7 +10,7 @@ export const LoginUser = async (user: {
   username: string
   password: string
 }): Promise<ResDataType> => {
-  const url = 'https://question-server.onrender.com/api/user/login'
+  const url = 'https://huy.wiki:3002/api/user/login'
   const data = (await instance.post(url, user)) as ResDataType
   return data
 }
@@ -20,7 +20,7 @@ export const RegisterUser = async (user: {
   password: string
   nickName?: string
 }) => {
-  const url = 'https://question-server.onrender.com/api/user/register'
+  const url = 'https://huy.wiki:3002/api/user/register'
   const data = (await instance.post(url, user)) as ResDataType
   return data
 }
