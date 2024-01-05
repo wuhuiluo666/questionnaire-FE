e7585edc4b54e8b49d4f0c5e67fc47e8c0be60cd
A reacter <136437794+wuhuiluo666@users.noreply.github.com>
2024-01-05 16:02:56 +0800

    Update static.ts

--- src/services/static.ts
+++ src/services/static.ts
@@ -9,7 +9,7 @@ export const getStaticListService = async (
   questionId: string,
   option: StaticOption
 ): Promise<ResDataType> => {
-  const url = `https://huy.wiki:3002/api/static/${questionId}`
+  const url = `https://huy.wiki:3002/api/stat/${questionId}`
   const data = (await instance.get(url, { params: option })) as ResDataType
   return data
 }
@@ -18,7 +18,7 @@ export const getChartListService = async (
   questionId: string,
   componentId: string
 ): Promise<ResDataType> => {
-  const url = `https://huy.wiki:3002/api/static/${questionId}/${componentId}`
+  const url = `https://huy.wiki:3002/api/stat/${questionId}/${componentId}`
   const data = (await instance.get(url)) as ResDataType
   return data
 }
